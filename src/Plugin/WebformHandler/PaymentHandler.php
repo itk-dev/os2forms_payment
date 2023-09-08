a3b5fcad2cf24f03a55ab67bed0bca7e476c64db
Jeppe Krogh <kjej@aarhus.dk>
2023-09-08 14:33:34 +0200

    moved files out of folder

---
+++ src/Plugin/WebformHandler/PaymentHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\webform_create_event\Plugin\WebformHandler;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\media\Entity\Media;
+use Drupal\file\Entity\File;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\WebformSubmissionInterface;
+
+
+/**
+ * Create a new node entity from a webform submission.
+ *
+ * @WebformHandler(
+ *   id = "Create a node",
+ *   label = @Translation("Create a node"),
+ *   category = @Translation("Entity Creation"),
+ *   description = @Translation("Creates a new node from Webform Submissions."),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_UNLIMITED,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_REQUIRED,
+ * )
+ */
+
+class CreateNodeWebformHandler extends WebformHandlerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function preSave(WebformSubmissionInterface $webform_submission) {
+
+  }
+
+}
